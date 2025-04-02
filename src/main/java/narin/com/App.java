988e83b88562e5f0fa456f770e9d738c1f9c3dcd
CHOR NARIN <naritkh178@gmail.com>
2025-04-02 10:10:32 +0700

    update new code

--- src/main/java/narin/com/App.java
+++ src/main/java/narin/com/App.java
@@ -6,5 +6,7 @@ package narin.com;
 public class App {
     public static void main(String[] args) {
         System.out.println("Hello World!");
+        System.out.println("Hello I am narin");
+
     }
 }
